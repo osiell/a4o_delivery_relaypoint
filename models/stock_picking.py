d530676c93c243417fa95a57a2c5c8f953c2b07b
Christophe <xt0ph@adiczion.net>
2020-12-10 10:11:15 +0100

    Added missing method.

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -2,6 +2,7 @@
 # The COPYRIGHT and LICENSE files at the top level of this repository
 # contains the full copyright notices and license terms.
 from odoo import api, models, fields, _
+from odoo.exceptions import UserError
 import logging
 
 _logger = logging.getLogger(__name__)
