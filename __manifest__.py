ffb96113a703a8398f8f471021e13c83331fdc02
Christophe <xt0ph@adiczion.net>
2022-03-12 20:31:28 +0100

    Release v15.1.0

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
 # contains the full copyright notices and license terms.
 {
     'name': 'Module Delivery Relay Points',
-    'version': '15.0.0',
+    'version': '15.1.0',
     'author': 'Adiczion SARL',
     'category': 'Adiczion',
     'license': 'AGPL-3',
