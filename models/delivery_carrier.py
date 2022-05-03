ce71ef9e808f3f1c245209048612a054d689310e
Christophe <xt0ph@adiczion.net>
2022-05-03 19:22:02 +0200

    PEP8

--- models/delivery_carrier.py
+++ models/delivery_carrier.py
@@ -11,7 +11,7 @@ _logger = logging.getLogger(__name__)
 class DeliveryCarrier(models.Model):
     _inherit = 'delivery.carrier'
     
-    @api.depends('delivery_type','relaypoint_delivery')
+    @api.depends('delivery_type', 'relaypoint_delivery')
     def _compute_delivery_relaypoint(self):
         for carrier in self:
             relaypoint_delivery = False
